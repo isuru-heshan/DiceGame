577f4623883baf083204297ce5b199f8390ec31a
Isuru Heshan <isuru.heshan1@gmail.com>
2022-08-10 12:17:40 +0530

    finished

--- script.js
+++ script.js
@@ -1,35 +1,59 @@
-let score1 = 0;
+let score1 = 0; /* to store the score */
 let score2 = 0;
-let flag = 1;
+let flag = 1; /* to indicate who is plating*/
+let newName1; /* name of the player */
+let newName2;
+/* get player names in the beginning */
+function setNames() {
+    newName1 = prompt("Enter name for player1");
+    newName2 = prompt("Enter name for player2");
+    /* check if name enterd or not*/
+    if (newName1.length > 0) {
+        document.getElementById("player1").innerHTML = newName1;
+        document.getElementById("turn").innerHTML = newName1;
+    }
+    if (newName2.length > 0) {
+        document.getElementById("player2").innerHTML = newName2;
+    }
+}
 
+/* call the function */
+setNames();
+
+/* function create roll animation and then to get dice values and */
 function rollDice(i = 6) {
 
+    /* check for reset button avilability if not avilable add reset button */
     let btnChk = document.querySelector('#btn2');
-    if (!btnChk){
+    if (!btnChk) {
         addReset();
     }
-
+    /* i used recursion function for animation, when recursion stops call getDiceVal to get random values for dice */
     if (i == 0) {
         getDiceVal();
         return;
     }
-    document.getElementById("dice-img2").src = "images/dice_" + i + ".png";
-    document.getElementById("dice-img1").src = "images/dice_" + i + ".png";
+    document.getElementById("dice-img2").src = "./images/dice_" + i + ".png";
+    document.getElementById("dice-img1").src = "./images/dice_" + i + ".png";
+    /* call function recursively */
     setTimeout(rollDice, 100, i - 1);
 }
-
+/* function for get random values for dice */
 function getDiceVal() {
-    let dice1 = 0;
+    let dice1 = 0; /* variable for store random values*/
     let dice2 = 0;
 
+    /* assign random values to variables (between 1 and 6) */
     dice1 = Math.round(Math.random() * 5) + 1;
     dice2 = Math.round(Math.random() * 5) + 1;
-
+    
+    /* change dice photo and display value according to the random value*/
     document.getElementById("diceVal1").innerHTML = "Dice 1 : " + dice1;
-    document.getElementById("dice-img1").src = "images/dice_" + dice1 + ".png";
+    document.getElementById("dice-img1").src = "./images/dice_" + dice1 + ".png";
     document.getElementById("diceVal2").innerHTML = "Dice 2 : " + dice2;
-    document.getElementById("dice-img2").src = "images/dice_" + dice2 + ".png";
+    document.getElementById("dice-img2").src = "./images/dice_" + dice2 + ".png";
 
+    /* add score to the right player */
     if (flag) {
         score1 = score1 + dice1 + dice2;
 
@@ -37,48 +61,52 @@ function getDiceVal() {
         score2 = score2 + dice1 + dice2;
     }
 
+    /* add rules to the game */
     if (dice1 != dice2 || (dice1 == dice2 && dice1 == 1)) {
-        if(dice1 == dice2 && dice1 == 1){
+        if (dice1 == dice2 && dice1 == 1) {
             if (flag) {
                 score1 = 0;
-            }else{
+            } else {
                 score2 = 0;
             }
         }
         flag = (flag + 1) % 2;
     }
 
+    /* call for the other functions */
     checkWinner();
     setFinalScore();
     checkPlayer();
 }
 
-function checkWinner(){
+/* first player to pass 100 is winner */
+function checkWinner() {
     if (score1 >= 100) {
-        alert("Player 1 Has Won!");
+        alert(newName1 + " Won!");
         restart();
     } else if (score2 >= 100) {
-        alert("Player 2 Has Won!");
+        alert(newName2 + " Won!");
         restart();
     }
 }
 
-function setFinalScore()
-{
+/* display final score after each round */
+function setFinalScore() {
     document.getElementById("score1").innerHTML = "Score : " + score1;
     document.getElementById("score2").innerHTML = "Score : " + score2;
 }
 
-function checkPlayer()
-{
+/* check whoes playing now and display */
+function checkPlayer() {
     if (flag) {
-        document.getElementById("turn").innerHTML = "Player 1";
+        document.getElementById("turn").innerHTML = newName1;
     } else {
-        document.getElementById("turn").innerHTML = "Player 2";
+        document.getElementById("turn").innerHTML = newName2;
     }
 }
 
-function addReset(){
+/* add restart button to the game */
+function addReset() {
     var btn2 = document.createElement("button");
     document.getElementById("div2").appendChild(btn2);
     btn2.innerText = "Restart Game";
@@ -86,15 +114,18 @@ function addReset(){
     btn2.addEventListener("click", reset);
 }
 
+/* When game is over disable roll button */
 function restart() {
     document.getElementById("btn1").disabled = true;
 }
 
+/* reset game */
 function reset() {
     score1 = 0;
     score2 = 0;
     flag = 1;
     setFinalScore();
-    checkPlayer()
+    checkPlayer();
     document.getElementById("btn1").disabled = false;
+    setNames();
 }
